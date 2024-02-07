830eced2265c81ed8ce647c708f127c345745a81
Joshua Covarrubias <joshuacovarrubias5@gmail.com>
2024-02-06 20:46:58 -0600

    Adding api-key file for instructions how to call spotify api

---
+++ api-key.py
@@ -0,0 +1,7 @@
+# Use this code to call the API in your gitbash
+#-------------------------------------
+#curl -X POST "https://accounts.spotify.com/api/token" \
+#     -H "Content-Type: application/x-www-form-urlencoded" \
+#     -d "grant_type=client_credentials&client_id=your-client-id&client_secret=your-client-secret"
+
+spotify_key = "BQBHl2orary4m4bnElXE0fJugfuXpW4wwSF2tIGI2HlQe8xvkoVtlPp73bp_RFmAARuDP2XeCCFuSBXWIhFGXWRALMdSQB-cYFF6SQpKswJYkduzGgQ"
