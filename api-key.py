ba04935fb7fb9340d064da3a644e9539e1fda4f2
Joshua Covarrubias <joshuacovarrubias5@gmail.com>
2024-02-09 23:09:03 -0600

    improving file path

--- api-key.py
+++
@@ -1,7 +0,0 @@
-# Use this code to call the API in your gitbash
-#-------------------------------------
-#curl -X POST "https://accounts.spotify.com/api/token" \
-#     -H "Content-Type: application/x-www-form-urlencoded" \
-#     -d "grant_type=client_credentials&client_id=bb8784cc254141239c34e4b1e00ba49e&client_secret=4a66810306e54c6e9d37ea46d586e2d2"
-
-spotify_key = "BQBHl2orary4m4bnElXE0fJugfuXpW4wwSF2tIGI2HlQe8xvkoVtlPp73bp_RFmAARuDP2XeCCFuSBXWIhFGXWRALMdSQB-cYFF6SQpKswJYkduzGgQ"
