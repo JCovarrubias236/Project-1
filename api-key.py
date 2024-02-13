cf345ec99d3f3a639aab48b74a63bdfa5da48967
IrynaVitkovych <viireni@gmail.com>
2024-02-13 16:02:00 -0600

    file api-key.py deleted

--- api-key.py
+++
@@ -1,9 +0,0 @@
-# Use this code to call the API in your gitbash
-#-------------------------------------
-#curl -X POST "https://accounts.spotify.com/api/token" \
-#     -H "Content-Type: application/x-www-form-urlencoded" \
-#     -d "grant_type=client_credentials&client_id=3e476130943a4d89abd801a74741bf0e&client_secret=0a7e6ea9dbc84274bb39e7dd8a91988c"
-
-
-
-spotify_key = "BQBXcGB2Gv6NfOzKWFr0z9POVS-Dxptsg9eISYU1pYOOJVnNjEsj6zrzpXO_73_Kq9nNzS9DfgHLDK-1fuUwcW5NMvNlFuO3YOoRwjAnY1cy0VNDHfA"
